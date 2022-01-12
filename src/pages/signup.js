d7141adf7784a1ef9456272f1eee0555a6f2109a
sangluong2k2 <luongduysang2k2@gmail.com>
2022-01-13 05:26:06 +0700

    update sign in, sign up

--- src/pages/signup.js
+++ src/pages/signup.js
@@ -1,6 +1,9 @@
 const signUp = {
     render() {
         return /*html*/ `
+        <div class="header">
+                ${Header.render()}
+            </div>
         <div class="min-h-full flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
         <div class="max-w-md w-full space-y-8">
           <div>
